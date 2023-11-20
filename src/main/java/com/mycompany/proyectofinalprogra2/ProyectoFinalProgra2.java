9b27908e69a74012812ee293b98532796662c3f2
AJRQ12UAM <149909684+AJRQ12UAM@users.noreply.github.com>
2023-11-20 17:49:05 -0600

    New Project

---
+++ src/main/java/com/mycompany/proyectofinalprogra2/ProyectoFinalProgra2.java
@@ -0,0 +1,12 @@
+package com.mycompany.proyectofinalprogra2;
+
+/**
+ *
+ * @author arodr/adri
+ */
+public class ProyectoFinalProgra2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
