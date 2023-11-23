f561e2a0f52f1329596341312f04c84445a7342d
AJRQ12UAM <149909684+AJRQ12UAM@users.noreply.github.com>
2023-11-22 18:50:35 -0600

    Adding SQL

--- src/main/java/com/mycompany/proyectofinalprogra2/Clases/SQLScripts.java
+++ src/main/java/com/mycompany/proyectofinalprogra2/Clases/SQLScripts.java
@@ -1,7 +1,21 @@
 package com.mycompany.proyectofinalprogra2.Clases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class SQLScripts {
-    //hello SQL
-    //263216
+  
 }
+
+public class Conexion {
+    public static Connection getConexion() {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:sqlserver://LaCompu\\SQLEXPRESS:1433;database=CRUDAutomoviles;"+ "encrypt=true;trustServerCertificate=true", "alejandro", "123456");
+            return con;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
